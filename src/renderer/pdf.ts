afa7e93073f27979b2bd47db2130b1e504c9d6ce
hata6502 <7702653+hata6502@users.noreply.github.com>
2024-09-01 00:16:11 +0900

    pdf.js esm

--- src/renderer/pdf.ts
+++ src/renderer/pdf.ts
@@ -1,35 +1,33 @@
 import * as pdfjsLib from "pdfjs-dist";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =
-  "../node_modules/pdfjs-dist/build/pdf.worker.js";
+  "../node_modules/pdfjs-dist/build/pdf.worker.mjs";
 
-(async () => {
-  const pdf = await pdfjsLib.getDocument(await electronAPI.getPDF()).promise;
-  for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber += 1) {
-    const page = await pdf.getPage(pageNumber);
-    const viewport = page.getViewport({ scale: 3 });
-    const canvasElement = document.createElement("canvas");
-    const canvasContext = canvasElement.getContext("2d");
-    if (!canvasContext) {
-      throw new Error("Couldn't get canvasContext. ");
-    }
-    canvasElement.width = Math.floor(viewport.width);
-    canvasElement.height = Math.floor(viewport.height);
-    canvasElement.style.width = `${Math.floor(viewport.width)}px`;
-    canvasElement.style.height = `${Math.floor(viewport.height)}px`;
-    await page.render({
-      canvasContext,
-      viewport,
-    }).promise;
-
-    const blob = await new Promise<Blob | null>((resolve) =>
-      canvasElement.toBlob(resolve)
-    );
-    if (!blob) {
-      throw new Error("Couldn't get blob. ");
-    }
-    await electronAPI.setPageImage(await blob.arrayBuffer());
+const pdf = await pdfjsLib.getDocument(await electronAPI.getPDF()).promise;
+for (let pageNumber = 1; pageNumber <= pdf.numPages; pageNumber += 1) {
+  const page = await pdf.getPage(pageNumber);
+  const viewport = page.getViewport({ scale: 3 });
+  const canvasElement = document.createElement("canvas");
+  const canvasContext = canvasElement.getContext("2d");
+  if (!canvasContext) {
+    throw new Error("Couldn't get canvasContext. ");
   }
+  canvasElement.width = Math.floor(viewport.width);
+  canvasElement.height = Math.floor(viewport.height);
+  canvasElement.style.width = `${Math.floor(viewport.width)}px`;
+  canvasElement.style.height = `${Math.floor(viewport.height)}px`;
+  await page.render({
+    canvasContext,
+    viewport,
+  }).promise;
 
-  await electronAPI.finishRenderPDF();
-})();
+  const blob = await new Promise<Blob | null>((resolve) =>
+    canvasElement.toBlob(resolve)
+  );
+  if (!blob) {
+    throw new Error("Couldn't get blob. ");
+  }
+  await electronAPI.setPageImage(await blob.arrayBuffer());
+}
+
+await electronAPI.finishRenderPDF();
